8033d2b8bda04f523c8297bf4a915ed9d427a61e
Lammerlord92 <lammerlord92@gmail.com>
2016-07-14 14:39:41 +0200

    Añadido movimiento del jugador

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundry
+{
+    public float xMin, xMax, zMin, zMax;
+} 
+
+public class PlayerController : MonoBehaviour {
+    public Rigidbody rb;
+    public float speed;
+    public float tilt;
+    public Boundry boundry;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento=new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movimiento * speed;
+
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundry.xMin, boundry.xMax), 
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundry.zMin, boundry.zMax)
+            );
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x*(-tilt));
+        
+    }
+
+}
