94ad2b8bb75d5654376c07dcc18fa79272509c84
Lammerlord92 <lammerlord92@gmail.com>
2016-08-01 12:37:07 +0200

    Añadidos asteroides que son destruidos por laser y se mueven hacia abajo, destruyendo al jugador en el caso de que haya contacto, aunque no se generan

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundry")
+        {
+            return;
+        }
+        //GameObject clone =
+        Instantiate(explosion, transform.position, transform.rotation); //as GameObject;
+
+        if (other.tag == "Player")
+        {
+            //GameObject clone =
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation); //as GameObject;
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
